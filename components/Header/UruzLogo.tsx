dc7af80652a041043c5516d3e02449ceed937e49
Sim Tzu Jye <simtj1990@gmail.com>
2022-11-20 16:53:08 +0800

    add landing link

--- components/Header/UruzLogo.tsx
+++ components/Header/UruzLogo.tsx
@@ -2,7 +2,12 @@ import { Flex, Heading, Text } from "@chakra-ui/react";
 
 export const UruzLogo = () => {
   return (
-    <Flex>
+    <Flex
+      as="a"
+      href="https://uruz-finance-near-landing.vercel.app/"
+      target="_blank"
+      cursor="pointer"
+    >
       <Heading fontSize="3xl">U R U Z</Heading>
     </Flex>
   );
